b43928015ffa72b54b9c9f7ee64e33919ce3e31a
Jasdeep <jasdeep.singh@spaceconcordia.ca>
2025-10-13 15:41:00 -0400

    fix migrations & login/register

--- server/prisma/migrations/20251013105255_fix_organization_id/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `organiztionId` on the `user` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `user` DROP COLUMN `organiztionId`,
-    ADD COLUMN `organizationId` VARCHAR(191) NULL;
