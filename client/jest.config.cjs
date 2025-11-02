2ec83b238f5a03d492ea7effab9e50e48be6ad19
karimikhaeil <135124371+karimikhaeil@users.noreply.github.com>
2025-11-02 18:34:19 -0500

    Fixing CI test

--- client/jest.config.cjs
+++ client/jest.config.cjs
@@ -4,11 +4,25 @@ module.exports = {
   testMatch: ["**/*.test.ts", "**/*.test.tsx"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+
   moduleNameMapper: {
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
     "\\.(svg|png|jpg|jpeg|gif)$": "<rootDir>/mocks/fileMock.js",
   },
+
   transform: {
-    "^.+\\.(ts|tsx|js|jsx)$": "babel-jest",
+    "^.+\\.(ts|tsx|js|jsx)$": [
+      "babel-jest",
+      {
+        presets: [
+          ["@babel/preset-env", { targets: { node: "current" } }],
+          ["@babel/preset-react", { runtime: "automatic" }],
+          "@babel/preset-typescript",
+        ],
+      },
+    ],
   },
+
+  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/index.{ts,tsx}"],
 };
