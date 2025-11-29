d9b4793387d759cfcae03109ebca868309c6ac0f
karimikhaeil <135124371+karimikhaeil@users.noreply.github.com>
2025-11-28 21:42:21 -0500

    fixed test

--- client/jest.setup.ts
+++ client/jest.setup.ts
@@ -1,4 +1,8 @@
 import "@testing-library/jest-dom";
+import { TextEncoder, TextDecoder } from "util";
+
+global.TextEncoder = TextEncoder as any;
+global.TextDecoder = TextDecoder as any;
 
 Object.defineProperty(globalThis as any, "import", {
   value: { meta: { env: { VITE_API_BASE_URL: "http://test.api" } } },
